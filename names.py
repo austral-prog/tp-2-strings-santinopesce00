8611f0add635a40f2febed0aa2928d5989afe226
santinopesce00 <spesce@mail.austral.edu.ar>
2026-03-17 12:23:33 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,12 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
+
+    nombre= input("¿Cual es tu nombre?")
+    apellido= input("¿Cual es tu apellido?")
+    nombre_completo = nombre + " " + apellido
+
+    print(nombre_completo.lower())
+    print(nombre_completo.title())
+    print(nombre_completo.upper())
+    print ("\t"+nombre_completo.lower())
